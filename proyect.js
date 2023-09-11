7d58eaba00903dc798d5354d022db4c4f297d7c4
SatanasIdol <kpenao@unal.edu.co>
2023-09-11 10:22:44 -0500

    Final

--- proyect.js
+++ proyect.js
@@ -1,51 +1,40 @@
-//Fibonacci
-function fibRec(n){
-  return n<=1 ?  n : (fibRec(n-2)+fibRec(n-1));
+//I Ching
+function rand(){
+  val = 0
+  while(val=0){
+    val = Math.floor(Math.random() * 4);
+  }
+  console.log(val)
 }
   
-function fibItera(n){
-  let a = 1;
-  let b = 1;
-  const fibSeries = n==1 ? [a] : [a,b];
-  
-  for (let i = 2; i < n; i++) {
-    nextFib = a + b;
-    fibSeries.push(nextFib);
-    a = b;
-    b = nextFib;
+// Función para generar una línea (yang o yin) de manera aleatoria
+function genLin() {
+  return Math.random() < 0.5 ? "yang" : "yin";
+}
+
+// Función para generar un hexagrama aleatorio
+function genHexa() {
+  const hexagram = [];
+  for (let i = 0; i < 6; i++) {
+    hexagram.push(genLin());
   }
-  return fibSeries;
+  return hexagram;
 }
 
-//Numero Aureo
-function aureoRec(n){
-  return fibRec(n+1)/fibRec(n);
-}
-
-function aureoItera(n) {
-  let a = 1;
-  let b = 1;
-
-  for (let i = 2; i <= n; i++) {
-    const nextAu = a + b;
-    a = b;
-    b = nextAu;
+// Función para mostrar el hexagrama
+function mostrarHexa(hexagram) {
+  for (let i = 0; i < 6; i++) {
+    const linea = hexagram[i];
+    simbolo = linea == "yang" ? "——————" : "——X——";
+    console.log(simbolo);
   }
-  return b/a;
 }
+
+// Generar y mostrar un hexagrama aleatorio
+console.log("Hexagrama Aleatorio:");
+mostrarHexa(genHexa());
+
+alert(`Hexagrama Aleatorio: ${mostrarHexa(genHexa())}`);
 
 // mensaje en pantalla
-alert("Hola, este programa calcula los primeros n terminos de la secuencia de Fibonacci, ademas "+
-      "calcula una aproximacion del numero aureo");
-x=true;
-
-// ingreso y validacion de datos
-while(x==true){
-  let n = prompt("Digite un numero");
-  (n > '0') && (n <= '9')
-  ? (n=parseInt(n),console.log(`Los primeros ${n} términos de la secuencia de Fibonacci son: ${fibItera(n)}`),
-  console.log(`La serie de Fibonacci hasta el término ${n} es: ${fibRec(n)}`),
-  console.log(`El número áureo calculado con los primeros ${n} términos de la serie de Fibonacci es aproximadamente: ${aureoItera(n)}`),
-  console.log("Otro redondeo del numero aureo: "+aureoRec(n)),x=false) 
-  : alert("Porfavor digite un valor valido ");
-}
+alert("Hola, este programa le muestra un hexagrama de el I Ching");
